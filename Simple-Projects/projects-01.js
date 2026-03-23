260e169b4ae43fc90a21f84ab2186d1f5a841ff4
Rakesh07778777 <bossrakesh43@gmail.com>
2026-03-23 22:43:09 +0530

    quiz

---
+++ Simple-Projects/projects-01.js
@@ -0,0 +1,10 @@
+const question = [
+    {
+        question: 'what will be 1 + 1',
+        answer: [
+            { text: '2' , guess: 'correct' },
+            { text: '9' , guess: 'wrong' },
+            { text: '6' , guess: 'wrong' },
+        ]
+    }
+]
--- random-image-generator/random.js
+++ random-image-generator/random.js
@@ -1,6 +1,15 @@
 const themeToggle = document.querySelector('.theme-toggle')
 
+(() => {
+   const savedTheme = localStorage.getItem('theme',)
+   const systemPrefersDark = window.matchMedia('(preference-color-scheme: dark)').matches
+   const isDarkTheme = savedTheme === 'dark' || (!darkTheme && systemPrefersDark)
+   document.body.classList.toggle('dark-theme', isDarkTheme)
+})();
+
 const toggleTheme = () => {
    const darkTheme =  document.body.classList.toggle('dark-theme')
+   localStorage.setItem('theme' , isDarkTheme ? 'light' : 'dark')
    themeToggle.querySelector('i').className = isDarkTheme ? 'fa-solid fa-sun' : 'fa-solid fa-moon';
 }
+
